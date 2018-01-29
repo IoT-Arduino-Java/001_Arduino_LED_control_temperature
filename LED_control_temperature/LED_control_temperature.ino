df85535ae8dabc2ff96fe240f3f3816c08836d54
Ionel <vuza@yahoo.com>
2018-01-29 17:00:52 +0100

    Update program
    - add defines for IR control
    - use buttons 1, 2 and 3 to set the active LED
    - rename variables according to type

--- LED_control_temperature/LED_control_temperature.ino
+++ LED_control_temperature/LED_control_temperature.ino
@@ -31,6 +31,28 @@ The active LED and the temperatured are shown on the LCD and on the UART.
 #include <LiquidCrystal_I2C.h>
 #include <IRremote.h>
 
+#define IR_CONTROL_CHMINUS   0xFFA25D 
+#define IR_CONTROL_CH        0xFF629D
+#define IR_CONTROL_CHPLUS    0xFFE21D
+#define IR_CONTROL_PREV      0xFF22DD
+#define IR_CONTROL_NEXT      0xFF02FD
+#define IR_CONTROL_PLAYPAUSE 0xFFC23D
+#define IR_CONTROL_VOL1      0xFFE01F
+#define IR_CONTROL_VOL2      0xFFA857
+#define IR_CONTROL_EQ        0xFF906F
+#define IR_CONTROL_BUTON0    0xFF6897
+#define IR_CONTROL_BUTON100  0xFF9867
+#define IR_CONTROL_BUTON200  0xFFB04F
+#define IR_CONTROL_BUTON1    0xFF30CF
+#define IR_CONTROL_BUTON2    0xFF18E7
+#define IR_CONTROL_BUTON3    0xFF7A85
+#define IR_CONTROL_BUTON4    0xFF10EF
+#define IR_CONTROL_BUTON5    0xFF38C7
+#define IR_CONTROL_BUTON6    0xFF5AA5
+#define IR_CONTROL_BUTON7    0xFF42BD
+#define IR_CONTROL_BUTON8    0xFF4AB5
+#define IR_CONTROL_BUTON9    0xFF52AD
+
 // IR variables
 int RECV_PIN = 11;
 IRrecv irrecv(RECV_PIN);
@@ -38,53 +60,50 @@ decode_results results;
 
 // constants won't change. They're used here to
 // set pin numbers:
-const int buttonPin = 2;    // the number of the pushbutton pin
-const int ledRosu   = 3;     // the number of the LED pin
-const int ledVer    = 4;
-const int ledAlb    = 5;
-const int sensorPin = A0;
+const int buttonPin   = 2;    // the number of the pushbutton pin
+const int intLedRed   = 3;    // the number of the LED pin
+const int intLedGreen = 4;
+const int intLedBlue  = 5;
+const int sensorPin   = A0;
 
       int ledactiv  = 1;
 
 // variables will change:
-int buttonState;     // variable for reading the pushbutton status
-int buttonStateOld;  // variable for keeping previous pushbutton status
+int intButtonState;     // variable for reading the pushbutton status
+int intButtonStateOld;  // variable for keeping previous pushbutton status
 
 
 //LCD
 LiquidCrystal_I2C lcd(0x3F,20,4);  // set the LCD address to 0x3F for a 16 chars and 2 line display
 
 //various
-int   taskCounter    = 0;
-float oldTemperature = 0;
-float temperature    = 1;
+int   intTaskCounter   = 0;
+float flTemperature    = 1;
+float flOldTemperature = 0;
+
 
 /************************************************************************************
  * 
  ************************************************************************************/
 void setup() {
 
-  lcd.init();                      // initialize the lcd 
+  // initialize the lcd 
+  lcd.init();
   lcd.backlight();
 
+  //init UART
   Serial.begin(9600); 
-  // initialize the LED pin as an output:
-  pinMode(ledRosu,   OUTPUT);
-  pinMode(ledVer,    OUTPUT);
-  pinMode(ledAlb,    OUTPUT);
+  
+  // initialize the LED pins as an output
+  pinMode(intLedRed,   OUTPUT);
+  pinMode(intLedGreen, OUTPUT);
+  pinMode(intLedBlue,  OUTPUT);
 
 
-  // initialize the pushbutton pin as an input:
+  // initialize the pushbutton pin as an input
   pinMode(buttonPin, INPUT);
 
-  // set RED on
-  digitalWrite(ledRosu, HIGH);
-  lcd.clear();
-  lcd.setCursor(0,1);
-  lcd.print("RED led is on!");
-
-  readTemperature();
-  irrecv.enableIRIn(); // Start the receiver
+  irrecv.enableIRIn(); // Start the IR receiver
   
   // Print the configuration
   Serial.print("Project description: \n ");
@@ -94,15 +113,24 @@ void setup() {
   Serial.print("Button Pin:     ");
   Serial.println(buttonPin, DEC);
   Serial.print("Red Led Pin:    ");
-  Serial.println(ledRosu, DEC);
+  Serial.println(intLedRed, DEC);
   Serial.print("Green Led Pin:  ");
-  Serial.println(ledVer, DEC);
+  Serial.println(intLedGreen, DEC);
   Serial.print("Blue Led Pin:   ");
-  Serial.println(ledAlb, DEC);
+  Serial.println(intLedBlue, DEC);
 
   Serial.print("\nStart ledactiv: ");
   Serial.println(ledactiv, DEC);
 
+
+  // set RED pin on
+  digitalWrite(intLedRed, HIGH);
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.print("RED led is on!");
+
+  // read temperature
+  readTemperature();
 }
 
 /************************************************************************************
@@ -110,19 +138,20 @@ void setup() {
  ************************************************************************************/
 void loop() {
 
-  buttonControlLED();
-  IRControlLED();
+  buttonControlLED(); // button Control
+  IRControlLED();     //IR Control
   
-  if (taskCounter == 0)
+  if (intTaskCounter == 0)
   {
-    readTemperature();
+    readTemperature(); // temperature read
   }
   
-  taskCounter++;
-  if (taskCounter > 9)
+  intTaskCounter++;
+  if (intTaskCounter > 9)
   {
-    taskCounter = 0;
+    intTaskCounter = 0;
   }
+  
   delay (100); // 1 second loop
 }
 
@@ -131,12 +160,12 @@ void loop() {
  ************************************************************************************/
 void changebutonPin()
 {
-     ledactiv = ledactiv + 1;
+  ledactiv = ledactiv + 1;
 
-    if (ledactiv == 4)
-    {
-      ledactiv = 1;
-    } 
+  if (ledactiv == 4)
+  {
+    ledactiv = 1;
+  } 
 }
 
 
@@ -145,25 +174,24 @@ void changebutonPin()
  ************************************************************************************/
 void readTemperature()
 {
-    // Read the sensor value
+  // Read the sensor value
   int sensorVal = analogRead(sensorPin);
   // Convert reading to voltage 
   float voltage = (sensorVal/1024.0) * 5.0;
-  //convert millivolts into temperature
-  temperature = (voltage - .5) * 100;
-
+  //convert millivolts into flTemperature
+  flTemperature = (voltage - .5) * 100;
   
-  if (oldTemperature != temperature)
+  if (flOldTemperature != flTemperature)
   {
-    oldTemperature = temperature;
+    flOldTemperature = flTemperature;
     lcd.setCursor(0, 0);
     lcd.print("Room Temp: ");
-    lcd.print(temperature);
+    lcd.print(flTemperature);
     Serial.print("Temperature:");
-    Serial.println(temperature, 6);
+    Serial.println(flTemperature, 6);
 
-    Serial.print("Old temperature: ");
-    Serial.println(oldTemperature, 6);
+    Serial.print("Old Temperature: ");
+    Serial.println(flOldTemperature, 6);
   }
 
 }
@@ -175,61 +203,85 @@ void readTemperature()
 void IRControlLED()
 {
 
+   // decode IR code
    if (irrecv.decode(&results))
    {
-      Serial.println(results.value, HEX);
-      Serial.println(results.value, DEC);
-      Serial.println(0xFFE21D, DEC);
-      Serial.println(0xFFA25D, DEC);
+      //Serial.println(results.value, HEX);
 
-      //CH++
-      if (results.value == 0xFFE21D){
-        Serial.print("CH++\n");
-        ledactiv++;
-    
-        if (ledactiv == 4)
+      switch (results.value)
+      {
+        case IR_CONTROL_CHMINUS: //CH--
         {
+          Serial.print("CH--IR_CONTROL_CHMINUS\n");
+          ledactiv--;
+      
+          if (ledactiv == 0)
+          {
+            ledactiv = 3;
+          }
+          break;
+        }
+
+        case IR_CONTROL_CHPLUS: //CH++
+        {
+          Serial.print("IR_CONTROL_CHPLUS\n");
+          ledactiv++;
+      
+          if (ledactiv == 4)
+          {
+            ledactiv = 1;
+          }
+          break;
+        }
+      
+        case IR_CONTROL_BUTON1:
+        {
+          Serial.print("IR_CONTROL_BUTON1\n");
           ledactiv = 1;
+          break;
         }
-      }
-
-      //CH--
-      if (results.value == 0xFFA25D){
-        Serial.print("CH--\n");
-        ledactiv--;
-    
-        if (ledactiv == 0)
+        case IR_CONTROL_BUTON2:
         {
-          ledactiv = 3;
+          Serial.print("IR_CONTROL_BUTON2\n");
+          ledactiv = 2;
+          break;
         }
-      }
+        case IR_CONTROL_BUTON3:
+        {
+          Serial.print("IR_CONTROL_BUTON3\n");
+          ledactiv = 3;
+          break;
+        }
 
+        default: // nothing here
+        break;
+      }
       Serial.print("ledactiv: ");
       Serial.println(ledactiv, DEC);
 
       if (ledactiv == 1)      // RED is on
       {
-         digitalWrite(ledRosu, HIGH);
-         digitalWrite(ledAlb,  LOW);
-         digitalWrite(ledVer,  LOW);
+         digitalWrite(intLedRed,   HIGH);
+         digitalWrite(intLedBlue,  LOW);
+         digitalWrite(intLedGreen, LOW);
 
-          lcd.setCursor(0,1);
+         lcd.setCursor(0,1);
          lcd.print("RED led is on!  ");
       }
       else if (ledactiv == 2) // WHITE is on
       {
-         digitalWrite(ledAlb,  HIGH); 
-         digitalWrite(ledVer,  LOW);
-         digitalWrite(ledRosu, LOW);
+         digitalWrite(intLedBlue,  HIGH); 
+         digitalWrite(intLedGreen, LOW);
+         digitalWrite(intLedRed,   LOW);
 
          lcd.setCursor(0,1);
          lcd.print("BLUE led is on! ");
       }
       else if (ledactiv == 3) // GREEN is on
       {
-         digitalWrite(ledVer,  HIGH);
-         digitalWrite(ledRosu, LOW);
-         digitalWrite(ledAlb,  LOW);
+         digitalWrite(intLedGreen, HIGH);
+         digitalWrite(intLedRed,   LOW);
+         digitalWrite(intLedBlue,  LOW);
 
          lcd.setCursor(0,1);
          lcd.print("GREEN led is on!");
@@ -245,20 +297,20 @@ void buttonControlLED()
 {
 
   // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
+  intButtonState = digitalRead(buttonPin);
 
-  //Serial.print("buttonState: ");
-  //Serial.println(buttonState, DEC);
+  //Serial.print("intButtonState: ");
+  //Serial.println(intButtonState, DEC);
  
   // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH){
+  // if it is, the intButtonState is HIGH:
+  if (intButtonState == HIGH){
    
     // update only on changed state
-    if(buttonStateOld == LOW){
+    if(intButtonStateOld == LOW){
 
       //update old button state
-      buttonStateOld = HIGH;
+      intButtonStateOld = HIGH;
       
       ledactiv = ledactiv + 1;
   
@@ -272,27 +324,27 @@ void buttonControlLED()
 
       if (ledactiv == 1)      // RED is on
       {
-         digitalWrite(ledRosu, HIGH);
-         digitalWrite(ledAlb,  LOW);
-         digitalWrite(ledVer,  LOW);
+         digitalWrite(intLedRed,   HIGH);
+         digitalWrite(intLedBlue,  LOW);
+         digitalWrite(intLedGreen, LOW);
 
-          lcd.setCursor(0,1);
+         lcd.setCursor(0,1);
          lcd.print("RED led is on!  ");
       }
       else if (ledactiv == 2) // WHITE is on
       {
-         digitalWrite(ledAlb,  HIGH); 
-         digitalWrite(ledVer,  LOW);
-         digitalWrite(ledRosu, LOW);
+         digitalWrite(intLedBlue,  HIGH); 
+         digitalWrite(intLedGreen, LOW);
+         digitalWrite(intLedRed,   LOW);
 
          lcd.setCursor(0,1);
          lcd.print("BLUE led is on! ");
       }
       else if (ledactiv == 3) // GREEN is on
       {
-         digitalWrite(ledVer,  HIGH);
-         digitalWrite(ledRosu, LOW);
-         digitalWrite(ledAlb,  LOW);
+         digitalWrite(intLedGreen,  HIGH);
+         digitalWrite(intLedRed,    LOW);
+         digitalWrite(intLedBlue,   LOW);
 
          lcd.setCursor(0,1);
          lcd.print("GREEN led is on!");
@@ -300,7 +352,7 @@ void buttonControlLED()
     }
   }
   else{
-   buttonStateOld = LOW;
+   intButtonStateOld = LOW;
   }
 }
 
